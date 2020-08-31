14df849aedb6c0b82e5e606f9dcf64f5a677fe48
Alex <53349658+AlexPavlenko91@users.noreply.github.com>
2020-08-31 12:51:14 +0300

    Add project files.

---
+++ Console_One_Neuron/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_One_Neuron
+{
+    class Program
+    {
+        public class Neuron
+        {
+            private decimal weight = 0.5m;
+            public decimal ProcessInputData(decimal input)
+            {
+                return input * weight;
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
